cb20a2034d3baa72b73e62ca969e045a6bd6c385
Vano0928 <vanyapro57@gmail.com>
2024-11-01 20:55:12 +0200

    vitalek gay

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,12 +1,12 @@
-import notification from './icons/notification.png';
-import reactLogo from './icons/react.svg';
-import dollar from './icons/icon-dollar-grey.svg';
-import bus from './icons/bus.png';
-import entertaiment from './icons/popcorn-box.png';
-import coin from './icons/earning.png';
-import utilities from './icons/electric-power.png';
-import shopping  from './icons/shopping-online.png';
-import food  from './icons/food.png';
+import notification from "./icons/notification.png";
+import reactLogo from "./icons/react.svg";
+import dollar from "./icons/icon-dollar-grey.svg";
+import bus from "./icons/bus.png";
+import entertaiment from "./icons/popcorn-box.png";
+import coin from "./icons/earning.png";
+import utilities from "./icons/electric-power.png";
+import shopping from "./icons/shopping-online.png";
+import food from "./icons/food.png";
 
 import graphIcon from "./icons/navbar/graph.png";
 import activeGraphIcon from "./icons/navbar/activeGraph.png";
@@ -20,20 +20,20 @@ import plusIcon from "./icons/navbar/plus.png";
 import userProfileIcon from "./icons/navbar/userAcc.png";
 
 export {
-    notification,
-    reactLogo,
-    dollar,
-    graphIcon,
-    activeGraphIcon,
-    homeIcon,
-    activeHomeIcon,
-    plusIcon,
-    activePlusIcon,
-    userProfileIcon,
-    bus,
-    entertaiment,
-    coin,
-    utilities,
-    shopping,
-    food
-  };
+  notification,
+  reactLogo,
+  dollar,
+  graphIcon,
+  activeGraphIcon,
+  homeIcon,
+  activeHomeIcon,
+  plusIcon,
+  activePlusIcon,
+  userProfileIcon,
+  bus,
+  entertaiment,
+  coin,
+  utilities,
+  shopping,
+  food,
+};
